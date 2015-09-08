a372a7ab30a0be71e7637deb70cdca6427afbe3f
Patrick Lynch <pelynch@wpi.edu>
2015-09-08 17:44:56 -0400

    Wrote an outline for a output function. Wrote a function for loading the world from a filename. pelynch

--- astar.py
+++ astar.py
@@ -1,7 +1,37 @@
 #!/usr/local/bin/python2.7
 
-def main():
+import sys
+
+class Location:
+    def __init__(self, complexity, type):
+        self.complexity = complexity
+        # type represents either start, goal, or a normal location
+        self.type = type
+
+def load_world(filename):
+    try:
+        world_file = open(filename, 'r')
+    except:
+        print('The file probably does not exist or something')
+        sys.exit(1)
+    world = []
+    for world_row in world_file:
+        row = []
+        for world_element in world_row.split("\t"):
+            row.append(int(world_element.rstrip("\n")))
+        world.append(row)
+    return world
+
+def output():
+    # The score of the path found
+    # The number of actions required to reach the goal
+    # The number of nodes expanded
+    # The series of actions (e.g., forward, turn, forward, forward, forward,
+    #   ...) taken to get to the goal, with each action separated by a newline
     pass
+
+def main():
+    load_world("test")
 
 if __name__ == "__main__":
     main()
