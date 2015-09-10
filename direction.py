4dc6b3b7cb0540e48035a3f57b0abdfa4156f4d1
alec <abenson@wpi.edu>
2015-09-10 09:55:08 -0400

    Fixes to direction

--- direction.py
+++ direction.py
@@ -1,19 +1,16 @@
-class Direction(tuple):
+class Direction():
     ''' The direction class is used to keep track of the agent's Forward position '''
     NORTH = (0, -1)
     EAST = (1, 0)
     SOUTH = (0, 1)
     WEST = (-1, 0)
 
-    def __new__(cls):
-        return super(Direction, cls).__new__(cls, Direction.NORTH)
-
     def __init__(self):
         self.index = 0
-        self.dirs = [self.NORTH, self.EAST, self.SOUTH, self.WEST]
+        self._dirs = [self.NORTH, self.EAST, self.SOUTH, self.WEST]
 
     def __getitem__(self, val):
-        return self.dirs[val % len(self.dirs)]
+        return self._dirs[val % len(self._dirs)]
 
     def turnLeft(self):
         self.index -= 1
@@ -22,3 +19,6 @@ class Direction(tuple):
     def turnRight(self):
         self.index += 1
         return self[self.index]
+
+    def direction(self):
+        return self._dirs[self.index]
