234c79a10c5ff5af9722d390904cb8f306754854
alec <abenson@wpi.edu>
2015-09-10 14:57:39 -0400

    Quick adjustments to path reconstruction

--- astar.py
+++ astar.py
@@ -88,15 +88,15 @@ class AStar:
 
         self.h = Heuristic(heuristic_num)
 
-        #Total cost to get to a given node
+        # Total cost to get to a given node
         self.cost_so_far = {}
         self.cost_so_far[self.world.start] = 0
 
-        #Openset is a priorityQueue where best options are first
+        # Openset is a priorityQueue where best options are first
         self.open_set = PriorityQueue()
         self.open_set.put(self.world.start, 0)
 
-        #cameFrom is a dictionary of the traversed path
+        # cameFrom is a dictionary of the traversed path
         self.came_from = {}
         self.came_from[self.world.start] = None
 
@@ -114,27 +114,47 @@ class AStar:
 
     def start(self):
         ''' Start the A star algorithm '''
-
         while not self.open_set.empty():
             current = self.open_set.get()
-
             if current == self.world.goal:
                 break
 
             for next in self.world.get_adjacent_cells(current):
-                new_cost = self.cost_so_far[current] + self.world.get_cell(next)
+                new_cost = self.cost_so_far[current] \
+                    + self.world.get_cell(next)
                 if next not in self.cost_so_far or new_cost < self.cost_so_far[next]:
                     self.cost_so_far[next] = new_cost
-                    priority = new_cost + self.h.estimate(self.world.goal, next)
+                    priority = new_cost + \
+                        self.h.estimate(next, self.world.goal)
                     self.open_set.put(next, priority)
                     self.came_from[next] = current
-        self.trace_came_from()
+        self.trace_path()
 
-    def trace_came_from(self):
-        previous = self.came_from[self.world.goal]
-        while previous is not None:
-            previous = self.came_from[previous]
-            print previous
+    def draw_solution(self, path):
+        ''' Draws the board and highlights the spaces that the agent took '''
+        green = '\033[92m'
+        reset = '\033[0m'
+        bold = '\033[1m'
+
+        for y, row in enumerate(self.world.rows):
+            print "\n"
+            for x, cell in enumerate(row):
+                if tuple([x, y]) in path:
+                    print "{0}{1}{2}{3}\t".format(bold, green, cell, reset),
+                else:
+                    print "{0}\t".format(cell),
+        print "\n"
+        print path
+
+    def trace_path(self):
+        '''Using the came_from dictionary, reconstruct the correct path to the goal '''
+        current = self.world.goal
+        path = [current]
+        while current != self.world.start:
+            current = self.came_from[current]
+            path.append(current)
+        path.reverse()
+        self.draw_solution(path)
 
 
 def main():
