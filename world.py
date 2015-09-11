08280a68c85023ad28630343e6af3de6f6496e97
Patrick Lynch <pelynch@wpi.edu>
2015-09-11 15:40:03 -0400

    Silly arrays, why can't you be like C#? pelynch

--- world.py
+++ world.py
@@ -122,5 +122,5 @@ class World:
             goal_y = random.randrange(height)
         self.start = (start_x, start_y)
         self.goal = (goal_x, goal_y)
-        self.rows[start_x][start_y] = Cell("S")
-        self.rows[goal_x][goal_y] = Cell("G")
+        self.rows[start_y][start_x] = Cell("S")
+        self.rows[goal_y][goal_x] = Cell("G")
